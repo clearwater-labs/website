eff17a525cc9301af30aa5d9c0997febe00a50f2
alex-stout <stoutab4384@uwec.edu>
2019-02-27 18:29:59 -0600

    adds routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Home from './components/Home'
+import './App.css'
 
-class App extends Component {
-  render() {
-    return <div className="App" />;
-  }
+class Routes extends Component {
+    render() {
+        return (
+            <Switch>
+                <Route path="/" exact component={Home} />
+            </Switch>
+        )
+    }
 }
 
-export default App;
+class App extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <Routes />
+            </BrowserRouter>
+        )
+    }
+}
+
+export default App
